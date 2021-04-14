b0536241f8cf05cfd04290bde8f8e944121ed8fc
majid <majidsawadi.fusion@gmail.com>
2021-04-14 10:44:58 +0200

    updating state

--- src/App.js
+++ src/App.js
@@ -1,71 +1,210 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import PublicNav from './components/PublicNav';
-import 'react-bootstrap'
-import ChoiceCard from './components/ChoiceCard';
-import { Container , Button } from 'react-bootstrap';
-
-const shapes = ["rock", "paper", "scissors"];
-let computerChoice;
-let playerChoice;
-const randomMove = () => {
-  computerChoice = shapes[Math.floor(Math.random() * 3)];
-  playerChoice = shapes[Math.floor(Math.random() * 3)];
-  console.log("Computer: ", computerChoice);
-  console.log("Player: ", playerChoice);
-};
-
-randomMove();
-let computerResult;
-let playerResult;
-const calculateWinner = () => {
-  if (computerChoice === playerChoice) {
-    computerResult = 'tie';
-    playerResult = 'tie';
-  } else if (computerChoice === 'rock') {
-    if (playerChoice === 'paper') {
-      computerResult = 'loss';
-      playerResult = 'win';
-    } else {
-      computerResult = 'win';
-      playerResult = 'loss';
-    }
-  } else if (computerChoice === 'paper') {
-    if (playerChoice === 'scissors') {
-      computerResult = 'loss';
-      playerResult = 'win';
-    } else {
-      computerResult = 'win';
-      playerResult = 'loss';
-    }
-  } else {
-    if (playerChoice === 'rock') {
-      computerResult = 'loss';
-      playerResult = 'win';
-    } else {
-      computerResult = 'win';
-      playerResult = 'loss';
-    }
-  }
-};
-
-const play = () => {
-  randomMove();
-  calculateWinner();
-};
-
-play();
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Button, ButtonGroup } from "react-bootstrap";
+import "./App.css";
+import ChoiceCard from "./components/ChoiceCard";
+import PublicNav from "./components/PublicNav";
+let streak = 0;
 function App() {
-  return (
-    <div className="App">
-      <PublicNav/>
-      <Container>
-      <ChoiceCard title="You" winner={playerResult} shape={playerChoice} />
-      <ChoiceCard title="Computer" winner={computerResult} shape={computerChoice} />
-      </Container>
-      <Button onClick={play}>Random</Button>
-    </div>
-  );
+	const shapes = ["rock", "paper", "scissors"];
+	const [playerName, setPlayerName] = useState("You");
+	const [playerChoice, setPlayerChoice] = useState("");
+	const [playerResult, setPlayerResult] = useState("tie");
+	const [playerScore, setPlayerScore] = useState(0);
+	const [computerChoice, setComputerChoice] = useState("");
+	const [computerResult, setComputerResult] = useState("tie");
+	const [computerScore, setComputerScore] = useState(0);
+  const[gameHistory , setGameHistory]=useState([{winner:"player", playerChoice:"peper" ,computerChoice:"rock"}]);
+
+	const randomMove = (move) => {
+		const newComputerChoice = shapes[Math.floor(Math.random() * 3)];
+
+		setPlayerChoice(move);
+		setComputerChoice(newComputerChoice);
+		calculateWinner(newComputerChoice, move);
+	};
+
+	const calculateWinner = (computerChoice, playerChoice) => {
+		if (computerChoice === playerChoice) {
+			setComputerResult("tie");
+			setPlayerResult("tie");
+      setGameHistory([...gameHistory,
+        {winner:"tie",
+      playerChoice,
+    computerChoice }
+      ]);
+			streak = 0;
+		} else if (computerChoice === "rock") {
+			if (playerChoice === "paper") {
+				if (streak >= 0) {
+					streak++;
+				} else {
+					streak = 1;
+				}
+				if (streak === 3) {
+					setPlayerResult("flawless victory");
+				} else {
+					setPlayerResult("win");
+				}
+				setComputerResult("loss");
+				setPlayerScore(playerScore + 1);
+        setGameHistory([...gameHistory,
+          {winner:"player",
+        playerChoice,
+      computerChoice }
+        ]);
+      
+			} else {
+				if (streak <= 0) {
+					streak--;
+				} else {
+					streak = -1;
+				}
+				if (streak === -3) {
+					setComputerResult("flawless victory");
+				} else {
+					setComputerResult("win");
+				}
+				setComputerScore(computerScore + 1);
+				setPlayerResult("loss");
+        setGameHistory([...gameHistory,
+          {winner:"computer",
+        playerChoice,
+      computerChoice }
+        ]);
+				streak--;
+			}
+		} else if (computerChoice === "paper") {
+			if (playerChoice === "scissors") {
+				if (streak >= 0) {
+					streak++;
+				} else {
+					streak = 1;
+				}
+				if (streak === 3) {
+					setPlayerResult("flawless victory");
+				} else {
+					setPlayerResult("win");
+				}
+				setComputerResult("loss");
+				setPlayerScore(playerScore + 1);
+        setGameHistory([...gameHistory,
+          {winner:"player",
+        playerChoice,
+      computerChoice }
+        ]);
+			} else {
+				if (streak <= 0) {
+					streak--;
+				} else {
+					streak = -1;
+				}
+				if (streak === -3) {
+					setComputerResult("flawless victory");
+				} else {
+					setComputerResult("win");
+				}
+
+				setComputerScore(computerScore + 1);
+				setPlayerResult("loss");
+        setGameHistory([...gameHistory,
+          {winner:"computer",
+        playerChoice,
+      computerChoice }
+        ]);
+			}
+		} else {
+			if (playerChoice === "rock") {
+				if (streak >= 0) {
+					streak++;
+				} else {
+					streak = 1;
+				}
+				if (streak === 3) {
+					setPlayerResult("flawless victory");
+				} else {
+					setPlayerResult("win");
+				}
+				setComputerResult("loss");
+
+				setPlayerScore(playerScore + 1);
+			} else {
+				if (streak <= 0) {
+					streak--;
+				} else {
+					streak = -1;
+				}
+				if (streak === -3) {
+					setComputerResult("flawless victory");
+				} else {
+					setComputerResult("win");
+				}
+				setComputerScore(computerScore + 1);
+				setPlayerResult("loss");
+        setGameHistory([...gameHistory,
+          {winner:"computer",
+        playerChoice,
+      computerChoice }
+        ]);
+			}
+		}
+	};
+
+	const restart = () => {
+		setPlayerChoice("");
+		setPlayerResult("tie");
+		setPlayerScore(0);
+		setComputerChoice("");
+		setComputerResult("tie");
+		setComputerScore(0);
+		streak = 0;
+	};
+
+	const handleChange = (e) => {
+		if (e.target.value) {
+			setPlayerName(e.target.value);
+		} else {
+			setPlayerName("You");
+		}
+	};
+	return (
+		<div className="App text-center">
+			<PublicNav />
+			<Container>
+				Name:
+				<input className="m-2" onChange={(e) => handleChange(e)}></input>
+				<div className="d-flex justify-content-center flex-wrap">
+					<ChoiceCard title={playerName} result={playerResult} shape={playerChoice} score={playerScore} />
+					<ChoiceCard title="Computer" result={computerResult} shape={computerChoice} score={computerScore} />
+				</div>
+				<ButtonGroup>
+					<Button variant="outline-dark" className="mx-1" onClick={() => randomMove("rock")}>
+						Play 👊
+					</Button>
+					<Button variant="outline-dark" className="mx-1" onClick={() => randomMove("paper")}>
+						Play 🤚
+					</Button>
+					<Button variant="outline-dark" className="mx-1" onClick={() => randomMove("scissors")}>
+						Play ✌
+					</Button>
+					<Button variant="dark" onClick={restart}>
+						RESTART
+					</Button>
+				</ButtonGroup>
+        <ul>
+          
+           {gameHistory.map((g, i)=>{
+           
+             return (
+             <div>
+             Game Number: {i+1}, winner:{g.winner}, playerChoice:{g.playerChoice}, computerChoice:{g.computerChoice}
+             </div>
+
+           );})};
+        </ul>
+			</Container>
+		</div>
+	);
 }
 
 export default App;
--- src/components/ChoiceCard.js
+++ src/components/ChoiceCard.js
@@ -4,30 +4,14 @@ import rock from "../images/rock.png";
 import paper from "../images/paper.png";
 import scissors from "../images/scisser.png";
 
-const ChoiceCard =(props)=>{
-    return(
-        <div
-      className={`choice-card ${
-        props.winner === "win"
-          ? "border-success"
-            ? props.winner === "loss"
-            : "border-danger"
-          : "border-dark"
-      }`}
-    >
-      <h2 className="text-center">{props.title}</h2>
-      <img
-        src={
-          props.shape === "rock"
-            ? rock
-            : props.shape === "paper"
-            ? paper
-            : scissors
-        }
-        alt={props.shape}
-      />
-    </div>
-    );
+const ChoiceCard = (props) => {
+	return (
+		<div className={`choice-card ${props.result === 'win' ? 'border-success' : props.result === 'tie' ? 'border-dark' : 'border-danger'}`}>
+			<h2>{props.title}</h2>
+			<img src={props.shape === 'rock' ? rock : props.shape === 'paper' ? paper : scissors} alt={props.shape} />
+			<h3>{props.result === 'win' ? 'WIN' : props.result === 'tie' ? 'TIE' : 'LOSS'}</h3>
+		</div>
+	);
 };
 
 export default ChoiceCard;
